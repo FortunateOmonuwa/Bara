107ecc6de809fc9d183c07cdfacb41af6b5ccf2f
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2025-07-28 23:41:06 +0100

    add mail notification templates

---
+++ Backend/Bara.API/Controllers/UserControllers/WriterController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedModule.Utils;
+using UserModule.DTOs.WriterDTOs;
+using UserModule.Interfaces.UserInterfaces;
+
+namespace Bara.API.Controllers.UserControllers
+{
+    [Route("api/writer")]
+    [ApiController]
+    public class WriterController : ControllerBase
+    {
+        private readonly IWriterService writerService;
+        private readonly ILogger<WriterController> logger;
+        public WriterController(IWriterService writerService, ILogger<WriterController> logger)
+        {
+            this.logger = logger;
+            this.writerService = writerService;
+        }
+
+        [HttpPost("AddWriter")]
+        public async Task<IActionResult> AddWriter([FromForm] PostWriterDetailDTO writerDetail)
+        {
+            try
+            {
+                if (writerDetail == null || !ModelState.IsValid)
+                {
+                    return BadRequest("Writer detail cannot be null");
+                }
+                var response = await writerService.AddWriter(writerDetail);
+                if (response.IsSuccess)
+                {
+                    return Ok(response);
+                }
+                else
+                {
+                    return BadRequest(response);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred while adding a writer.");
+                return (IActionResult)ResponseDetail<IActionResult>.Failed(ex.Message, 500);
+            }
+        }
+    }
+}
--- Backend/Bara.API/Program.cs
+++ Backend/Bara.API/Program.cs
@@ -1,4 +1,6 @@
 using Infrastructure.DataContext;
+using Infrastructure.Repositories.FileRepositories;
+using Infrastructure.Repositories.UserRepositories;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 using Services.ExternalAPI_Integration;
@@ -6,6 +8,8 @@ using Services.FileStorageServices.CloudinaryStorage;
 using Services.FileStorageServices.Interfaces;
 using Services.YouVerifyIntegration;
 using SharedModule.Settings;
+using System.Text.Json.Serialization;
+using UserModule.Interfaces.UserInterfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +25,12 @@ builder.Services.AddDbContext<BaraContext>(options =>
 builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
 builder.Services.Configure<Secrets>(builder.Configuration.GetSection("Secrets"));
 
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+builder.Services.AddMemoryCache();
+
 builder.Host.UseSerilog((context, config) => config.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddMemoryCache();
@@ -28,6 +38,9 @@ builder.Services.AddMemoryCache();
 builder.Services.AddScoped<IYouVerifyService, YouVerifyService>();
 builder.Services.AddScoped<ExternalApiIntegrationService>();
 builder.Services.AddScoped<IFileStorageService, CloudinaryService>();
+builder.Services.AddScoped<IFileService, FileRepository>();
+
+builder.Services.AddScoped<IWriterService, WriterRepository>();
 
 builder.Services.AddHttpClient("YouVerify", client =>
 {
